a13c987ef992c77ffbd71302b4fa9d5e9342840b
HN41A11 <63926997+HN41A11@users.noreply.github.com>
2020-07-18 04:35:49 +0800

    Update response.go
    emp1

--- response.go
+++ response.go
@@ -8,8 +8,20 @@ import (
 type tokenResponse struct {
 	Status      string `json:"Name"`
 	Message     string `json:"message"`
-	AccessToken string 'Vm7SSB93huTitkRAlq95213229S82QYzNIdn1rJXN4q'
+	AccessToken string `json:"access_token"`
 }
+
+json_string := `
+    {
+        "Name": "200",
+        "message": "access_token is issued",
+        "access_token": "Vm7SSB93huTitkRAlq95213229S82QYzNIdn1rJXN4q"
+    }`
+
+emp1 := new(tokenResponse)
+
+json.Unmarshal([]byte(json_string), emp1)
+
 /*
 type tokenResponse struct {
 	Status      string `json:"Name"`
@@ -20,6 +32,7 @@ type tokenResponse struct {
 
 func newTokenResponse(raw []byte) *tokenResponse {
 	ret := &tokenResponse{}
+	ret := &emp1{}
 	err := json.Unmarshal(raw, &ret)
 	if err != nil {
 		log.Println("json unmarshal err:", err)
